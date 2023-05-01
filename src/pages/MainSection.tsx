9c36a7d1dd4742fe8b466c47ccd79ee73842e2d8
ahnsunnyyoung <ahnsunnyyoung@gmail.com>
2023-05-01 01:54:31 +0100

    Setup interactive scroll section view

---
+++ src/pages/MainSection.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function FirstSection() {
+  return (
+    <section>
+      <h1>Hello, I'm Sunyoung Ahn &#8595;</h1>
+    </section>
+  )
+}
---
+++ src/pages/SecondSection.tsx
@@ -0,0 +1,33 @@
+
+export default function SecondSection() {
+  return (
+    <section>
+        <div className="slide reveal">
+          <h2>Caption</h2>
+          <div className="text-container">
+            <div className="text-box">
+              <h3>Section Text</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+                eius molestiae perferendis eos provident vitae iste.
+              </p>
+            </div>
+            <div className="text-box">
+              <h3>Section Text</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+                eius molestiae perferendis eos provident vitae iste.
+              </p>
+            </div>
+            <div className="text-box">
+              <h3>Section Text</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+                eius molestiae perferendis eos provident vitae iste.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+  )
+}
