8432b6f5c3f7ecca81aeaa73c825ed31151e5e0f
ahnsunnyyoung <ahnsunnyyoung@gmail.com>
2023-05-03 14:01:24 +0100

    Applied animation from greensocks

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,10 +1,58 @@
-import Link from 'next/link';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { MdSunny } from 'react-icons/md';
 import { theme } from '../../styles';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger); 
 
 export default function Header() {
   const [navbar, setNavbar] = useState(false);
+
+  useEffect(() => {
+    gsap.to("#logo-icon", {
+      rotate:'360',
+      duration:3,
+    });
+
+    gsap.to("#logo", {
+      x:'40%',
+      y:'-40%',
+      duration:3,
+      scrollTrigger: {
+        trigger: "#belief",
+        start: "0px 100%",
+        end: "0px 0%",
+        scrub: true,
+        toggleActions: 'play pause reverse pause'
+      },
+    });
+
+    gsap.to("#logo", {
+      x:'80%',
+      y:'0%',
+      immediateRender: false,
+      duration:3,
+      scrollTrigger: {
+        trigger: "#projects",
+        start: "0px 100%",
+        end: "0px 0%",
+        scrub: true,
+        toggleActions: 'play pause reverse pause'
+      }
+    });
+
+    gsap.to("#logo", {
+      opacity: 0,
+      immediateRender: false,
+      scrollTrigger: {
+        trigger: "#contact",
+        start: "0px 100%",
+        end: "0px 0%",
+        scrub: true,
+        toggleActions: 'play pause reverse pause'
+      }
+    });
+  }, []);
 
   const scrollTo = (id: string, e: { preventDefault: () => void; }) => {
     const section = document.querySelector( id );
@@ -12,58 +60,58 @@ export default function Header() {
       section.scrollIntoView( { behavior: 'smooth', block: 'start' } );
     }
   }
-
   return (
     <nav className="w-full bg-transparent" id='navbar'>
-      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-        <div>
-          <div className="flex items-center justify-between py-3 md:py-5" onClick={(e)=>{scrollTo('#home', e)}}>
-            <div id="logo-icon">
-              <MdSunny color={theme.palette.darkpink} size='100%'/>
-            </div>
-            <div id="logo-text">
-              <p id="txt-sunny">SUNNY</p>
-              <p id="txt-world">world</p>
-            </div>
-            <div className="md:hidden">
-              <button
-                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                onClick={() => setNavbar(!navbar)}
-              >
-                {navbar ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                )}
-              </button>
+      <div style={{width:'100%'}} className="flex items-center justify-between p-3 md:py-5">
+          <div  id="logo-container">
+            <div id="logo" className='flex align-content-center' onClick={(e)=>{scrollTo('#home', e)}}>
+              <div id="logo-icon">
+                <MdSunny color={theme.palette.darkpink} size='100%'/>
+              </div>
+              <div id="logo-text">
+                <p id="txt-sunny">SUNNY</p>
+                <p id="txt-world">world</p>
+              </div>
             </div>
           </div>
+          {/* <div className="md:hidden">
+            <button
+              className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+              onClick={() => setNavbar(!navbar)}
+            >
+              {navbar ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </button>
+          </div> */}
         </div>
-        <div>
+        {/* <div style={{float:'right'}}>
           <div
             className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
               navbar ? 'block' : 'hidden'
@@ -92,8 +140,7 @@ export default function Header() {
               </li>
             </ul>
           </div>
-        </div>
-      </div>
+        </div> */}
     </nav>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, } from "react";
 import { Inter } from 'next/font/google'
 import Header from './components/Header';
 import MainSection from './components/MainSection';
 import FirstSection from './components/FirstSection';
 import SecondSection from './components/SecondSection';
 import ThirdSection from './components/ThirdSection';
+import { IoMdFlower } from 'react-icons/io';
 
+const scrollTo = (id: string, e: { preventDefault: () => void; }) => {
+  const section = document.querySelector( id );
+  if (section){
+    section.scrollIntoView( { behavior: 'smooth', block: 'start' } );
+  }
+}
 
 export default function Home() {
   const options = {
@@ -26,11 +33,7 @@ export default function Home() {
     }, options);
     
     document.querySelectorAll('.reveal').forEach(el => observer.observe(el));
-
   }, []);
-
-  
-  
 
   return (
     <div className="main-container">
@@ -39,6 +42,21 @@ export default function Home() {
       <FirstSection/>
       <SecondSection/>
       <ThirdSection/>
+      <div id="navigator-dots">
+        <div id="dots" onClick={(e)=>{scrollTo('#home', e)}}>
+          <IoMdFlower/>
+        </div>
+        <div id="dots" onClick={(e)=>{scrollTo('#belief', e)}}>
+          <IoMdFlower/>
+        </div>
+        <div id="dots" onClick={(e)=>{scrollTo('#projects', e)}}>
+          <IoMdFlower/>
+        </div>
+        <div id="dots" onClick={(e)=>{scrollTo('#contact', e)}}>
+          <IoMdFlower/>
+        </div>
+
+      </div>
     </div>
   )
 }
