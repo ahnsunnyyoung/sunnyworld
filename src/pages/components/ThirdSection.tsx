4a291592719d9141819305a1877bb3ac58af8150
ahnsunnyyoung <ahnsunnyyoung@gmail.com>
2023-05-03 22:03:58 +0100

    Changed contact info as a flex view

--- src/pages/components/ThirdSection.tsx
+++ src/pages/components/ThirdSection.tsx
@@ -16,34 +16,48 @@ export default function ThirdSection() {
     <section id='contact'>
         <div className="slide reveal">
           <div className='contact-container'>
-            <Grid container spacing={2} className="text-center">
-              <Grid item xs={4} className="contact-icon">
-                <div className='flex justify-center'><HiLocationMarker size='2rem'/></div>
-              </Grid>
-              <Grid item xs={4} className="contact-icon">
-                <div className='flex justify-center'><HiMail size='2rem'/></div>
-              </Grid>
-              <Grid item xs={4} className="contact-icon">
-                <div className='flex justify-center'><HiPhone size='2rem'/></div>
-              </Grid>
-              <Grid item xs={4} className="contact-text">
-                Dublin, Ireland
-              </Grid>
-              <Grid item xs={4} className="contact-text">
-                ahnsunnyyoung@gmail.com
-              </Grid>
-              <Grid item xs={4} className="contact-text">
-                +353 &#40;087&#41; 676 3674
-              </Grid>
-            </Grid>
+            <div className="contact-table">
+              <div className="contact-col">
+                <div className='flex justify-center' id='contact-icon'><HiLocationMarker size='2rem'/></div>
+                <div className='flex justify-center' id='contact-text'>Dublin, Ireland</div>
+              </div>
+              <div className="contact-col">
+              <div className='flex justify-center' id='contact-icon'><HiPhone size='2rem'/></div>
+                <div className='flex justify-center' id='contact-text'>ahnsunnyyoung@gmail.com</div>
+              </div>
+              <div className="contact-col">
+              <div className='flex justify-center' id='contact-icon'><HiPhone size='2rem'/></div>
+                <div className='flex justify-center' id='contact-text'>+353 &#40;087&#41; 676 3674</div>
+              </div>
+            </div>
           </div>
           
-          {/* <div className="logo-container">
+          <div className="icon-container">
             <div className="icon-box">
-              <MdSunny color={theme.palette.darkpink} size='100%'/>
+              <MdSunny id="contact-icon-svg" color={theme.palette.darkpink} size='40%'/>
             </div>
-          </div> */}
+          </div>
+
+          
+          <div className='contact-container'>
+            <div className="contact-table">
+              <div className="contact-col">
+                <div className='flex justify-center' id='contact-icon'><HiLocationMarker size='2rem'/></div>
+                <div className='flex justify-center' id='contact-text'>Dublin, Ireland</div>
+              </div>
+              <div className="contact-col">
+              <div className='flex justify-center' id='contact-icon'><HiPhone size='2rem'/></div>
+                <div className='flex justify-center' id='contact-text'>ahnsunnyyoung@gmail.com</div>
+              </div>
+              <div className="contact-col">
+              <div className='flex justify-center' id='contact-icon'><HiPhone size='2rem'/></div>
+                <div className='flex justify-center' id='contact-text'>+353 &#40;087&#41; 676 3674</div>
+              </div>
+            </div>
+          </div>
+          
         </div>
+
       </section>
   )
 }
