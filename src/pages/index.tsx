174be0e2cb89412836f7626f446375eaab2569cc
ahnsunnyyoung <ahnsunnyyoung@gmail.com>
2023-05-04 02:40:58 +0100

    Add title and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, } from "react";
+import Head from 'next/head'
 import Header from './components/Header';
 import MainSection from './components/MainSection';
 import FirstSection from './components/FirstSection';
@@ -36,6 +37,13 @@ export default function Home() {
 
   return (
     <div className="main-container">
+      <Head>
+        <title>Sunny World</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </Head>
       <Header/>
       <MainSection/>
       <FirstSection/>
